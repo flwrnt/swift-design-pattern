ca60d47b2ce6464c5df28168a786d7939f901b4e
Florent AUPLAT <florent.auplat@gmail.com>
2016-11-20 21:39:44 +0100

    migrate notifications core data handler in specific class

--- design-pattern/Notification.swift
+++ design-pattern/Notification.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import CoreData
 
 protocol Mappable {
     static func mapToModel(_ o: Any) -> Result<Self>
@@ -15,25 +14,21 @@ protocol Mappable {
 
 
 final class Notification: Mappable, Equatable, CustomStringConvertible {
-    private var id: String
-    private var title: String
-    private var body: String
-    private var receiveDate: Date
+    var id: String
+    var title: String
+    var body: String
+    var receiveDate: Date
     
     // conform to custom string convertible protocol
     public var description: String {
         return "notification n° \(id) : \n\t title: \(title) \n\t body: \(body) \n\t date: \(receiveDate)"
     }
     
-    let moc: NSManagedObjectContext
-    
     init(id: String = UUID().uuidString, title: String, body: String, receiveDate: Date = Date()) {
         self.id = id
         self.title = title
         self.body = body
         self.receiveDate = receiveDate
-        
-        moc = AppDelegate.getMoc()!
     }
     
     // conform to mappable ptotocol
@@ -49,64 +44,8 @@ final class Notification: Mappable, Equatable, CustomStringConvertible {
         return .fail(.parser)
     }
     
-    // accessors
-    public func getId() -> String {
-        return id
-    }
-    
-    public func getTitle() -> String {
-        return title
-    }
-    
-    public func getBody() -> String {
-        return body
-    }
-    
-    public func getReceiveDate() -> Date {
-        return receiveDate
-    }
-    
     // conform to equatable protocol
     static func ==(lhs: Notification, rhs: Notification) -> Bool {
         return lhs.id == rhs.id && lhs.title == rhs.title && lhs.body == rhs.body
-    }
-}
-
-// MARK: - handle core data notifications
-
-extension Notification {
-    func save() {
-        let entity = NSEntityDescription.insertNewObject(forEntityName: "Notifications", into: moc) as! Notifications
-        
-        entity.id = self.getId()
-        entity.title = self.getTitle()
-        entity.body = self.getBody()
-        entity.receive_date = self.getReceiveDate() as NSDate
-        
-        do {
-            try moc.save()
-            print(Log("notification saved"))
-        } catch {
-            fatalError(Log("save notification error: \(error)").description)
-        }
-    }
-    
-    class func save(_ notifications: [Notification]) {
-        for notification in notifications {
-            notification.save()
-        }
-    }
-    
-    class func fetchAll() -> [Notifications] {
-        let moc = AppDelegate.getMoc()!
-        let request: NSFetchRequest = NSFetchRequest<Notifications>(entityName: "Notifications")
-        
-        do {
-            return try moc.fetch(request).sorted {
-                $0.receive_date!.timeIntervalSince1970 > $1.receive_date!.timeIntervalSince1970
-            }
-        } catch {
-            fatalError(Log("fetch notifications error: \(error)").description)
-        }
     }
 }
---
+++ design-pattern/NotificationsCoreDataStoreService.swift
@@ -0,0 +1,54 @@
+//
+//  NotificationsCoreDataStore.swift
+//  design-pattern
+//
+//  Created by Flwrnt on 20/11/2016.
+//  Copyright © 2016 Flwrnt. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class NotificationCoreDataStore {
+    let moc: NSManagedObjectContext
+    
+    init() {
+        moc = AppDelegate.getMoc()!
+    }
+    
+    func fetchNotifications(_ completionHandler: (Result<[Notifications]>) -> Void) {
+        let request: NSFetchRequest = NSFetchRequest<Notifications>(entityName: "Notifications")
+        
+        do {
+            let notifications = try moc.fetch(request).sorted {
+                $0.receive_date!.timeIntervalSince1970 > $1.receive_date!.timeIntervalSince1970
+            }
+            return completionHandler(.success(notifications))
+        } catch {
+            print(Log("fetch notifications error: \(error)").description)
+            return completionHandler(.fail(.coreData("error while fetching notifications")))
+        }
+    }
+    
+    func saveNotification(_ notification: Notification) {
+        let entity = NSEntityDescription.insertNewObject(forEntityName: "Notifications", into: moc) as! Notifications
+        
+        entity.id = notification.id
+        entity.title = notification.title
+        entity.body = notification.body
+        entity.receive_date = notification.receiveDate as NSDate
+        
+        do {
+            try moc.save()
+            print(Log("notification saved"))
+        } catch {
+            fatalError(Log("save notification error: \(error)").description)
+        }
+    }
+    
+    func saveNotifications(_ notifications: [Notification]) {
+        for notification in notifications {
+            self.saveNotification(notification)
+        }
+    }
+}
